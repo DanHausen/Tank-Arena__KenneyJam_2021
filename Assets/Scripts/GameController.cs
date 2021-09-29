b33ec5b9a10e41006a73ebc9d20255aa5d35fcc9
Danhausen <danihausen@live.com>
2021-09-29 19:32:51 -0300

    Update

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,23 +1,21 @@
-using System.Net.Mime;
-using System.Linq.Expressions;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     [SerializeField] private int ammoAmount = 3;
-    [SerializeField] private Text ammoCounterTextField;    
+    [SerializeField] private Text scorePointsText;    
     [SerializeField] private GameObject bullet_Spawn_Point;
     [SerializeField] private GameObject bullet;    
+    
+    public Slider ammoSlider;
     
     //TODO Criar mecanica para spawnar inimigos neste script e tambem evoluir na dificuldade
     //TODO Aqui tambem vai o temporizador e leaderboard
     //TODO seria bacana adicionar power ups
     
     void Start(){
-        AmmoCounter(ammoAmount);
+        AmmoCounterSliderUpdate(ammoAmount);
     }
     
     public bool FiredBullet(){
@@ -25,13 +23,13 @@ public class GameController : MonoBehaviour
             GameObject temporary_Rigidbody2D;
             temporary_Rigidbody2D = Instantiate(bullet, bullet_Spawn_Point.transform.position, bullet_Spawn_Point.transform.rotation);
             ammoAmount--;
-            AmmoCounter(ammoAmount);
+            AmmoCounterSliderUpdate(ammoAmount);
         }
             return true ? ammoAmount > 0 : false;
     }
     //TODO Adicionar um temporizador de tiro e uma quantidade de munição
     
-    private void AmmoCounter(int ammo){
-        ammoCounterTextField.text = ammo.ToString();
+    private void AmmoCounterSliderUpdate(int ammo){
+        ammoSlider.value = ammo;
     }
 }
