20fa0b2a2dbd2a32d8b8d8164543b93532f5f61a
Danhausen <danihausen@live.com>
2021-09-29 19:51:23 -0300

    Update

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,7 +8,11 @@ public class GameController : MonoBehaviour
     [SerializeField] private GameObject bullet_Spawn_Point;
     [SerializeField] private GameObject bullet;    
     
+    private bool fireTimerBlocked = false;
+    
     public Slider ammoSlider;
+    public int ammoMax = 3;
+    public float fillAmount = 0.1f;
     
     //TODO Criar mecanica para spawnar inimigos neste script e tambem evoluir na dificuldade
     //TODO Aqui tambem vai o temporizador e leaderboard
@@ -18,6 +22,10 @@ public class GameController : MonoBehaviour
         AmmoCounterSliderUpdate(ammoAmount);
     }
     
+    void Update(){
+        AmmoRefill();
+    }
+    
     public bool FiredBullet(){
         if (ammoAmount > 0){
             GameObject temporary_Rigidbody2D;
@@ -25,11 +33,24 @@ public class GameController : MonoBehaviour
             ammoAmount--;
             AmmoCounterSliderUpdate(ammoAmount);
         }
-            return true ? ammoAmount > 0 : false;
+            return AmmoTimmerBlocker();
     }
-    //TODO Adicionar um temporizador de tiro e uma quantidade de munição
     
     private void AmmoCounterSliderUpdate(int ammo){
         ammoSlider.value = ammo;
+    }
+    
+    //TODO Adicionar um temporizador de tiro    
+    private bool AmmoTimmerBlocker(){
+        return false ? ammoAmount <= 0 : true;
+    }
+    
+    private void AmmoRefill(){
+        if(ammoAmount < ammoMax){//TODO Não está funcionando
+            ammoSlider.value += fillAmount * Time.deltaTime;
+        }
+        else{
+            fireTimerBlocked = AmmoTimmerBlocker();
+        }
     }
 }
