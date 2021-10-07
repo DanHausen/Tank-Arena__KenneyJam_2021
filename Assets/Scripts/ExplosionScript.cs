2fc58b724f69b73d2f031110bf3f91f5adc1ac44
Danhausen <danihausen@live.com>
2021-10-07 20:38:49 -0300

    Quick update

--- Assets/Scripts/ExplosionScript.cs
+++ Assets/Scripts/ExplosionScript.cs
@@ -1,15 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ExplosionScript : DestroyAndInstantiateObject
 {
     public float timeToDestroy = 4f;
+    public float explosionRadiusSize2D = 1f;
+    private CircleCollider2D circleExplosionCollider2D;
     
     void Start()
-    {
-        //TODO executar animação
-        
+    {        
+        circleExplosionCollider2D = GetComponent<CircleCollider2D>();
+        circleExplosionCollider2D.radius = explosionRadiusSize2D;
         DestroyGameObject(timeToDestroy);
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+        //TODO Trigger para explodir o que entrar no caminho da explosão do missile
     }
 }
