2a6160e386583e7d878a4e764849a5caf449fe6c
Danhausen <danihausen@live.com>
2021-10-08 22:04:47 -0300

    Update

--- Assets/Scripts/ExplosionScript.cs
+++ Assets/Scripts/ExplosionScript.cs
@@ -15,5 +15,6 @@ public class ExplosionScript : DestroyAndInstantiateObject
     
     private void OnTriggerEnter2D(Collider2D other) {
         //TODO Trigger para explodir o que entrar no caminho da explosão do missile
+        Debug.Log("Explosão");
     }
 }
