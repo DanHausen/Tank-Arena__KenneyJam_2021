f44eda01c50fc540f544994edfe5d12db59be135
Danhausen <danihausen@live.com>
2021-10-23 19:35:45 -0300

    TODOs

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    //TODO 5 Criar mecanica para IA dos inimigos
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,15 +7,14 @@ public class GameController : MonoBehaviour
     private int _ammoMax = 3;
     private float _timeToReload = 2f;
     
-    public Image _image;    
+    public Image image;
     public float timeToReload = 2f;
     public Slider ammoSlider;
     
     void Start(){
         AmmoCounterSliderUpdate(PlayerMovement._ammoAmount);
         _timeToReload = timeToReload;
-        _image = _image.GetComponent<Image>();
-        
+        image = image.GetComponent<Image>();
     }
     
     void Update(){
@@ -43,33 +42,33 @@ public class GameController : MonoBehaviour
     }
     
     private void EnemySpawn(){
-        //TODO Criar mecanica para spawnar inimigos neste script e tambem evoluir na dificuldade. 2 tipos de inimigos (sniper e bomber)
-        //Sniper - Fica fugindo do jogador e atirando de longe
-        //Bomber - Corre até o jogador e explode
-        
-        //TODO Adicionar animação de explosão do kit do Kenney. Removendo as particulas que inseri.
-        
-        //TODO Criar mecanica para IA dos inimigos
-        
-        //TODO Criar menu principal e de pausa
-        
-        //TODO eliminar o reload automatico de munição e adicionar algo que o jogador precisa coletar, ou seja, vai precisar se posicionar corretamente e alinhar seus tiros.
-    }    
+        //TODO 3 Criar mecanica para spawnar inimigos. 2 tipos de inimigos (sniper e bomber)
+            //Sniper - Fica fugindo do jogador e atirando de longe
+            //Bomber - Corre até o jogador e explode
+    }
     
     private void SliderColorUpdater(float ammo){
         switch(ammo){
             case 0:
-                _image.color = Color.red;
+                image.color = Color.red;
                 break;
             case 1:
-                _image.color = Color.red;
+                image.color = Color.red;
                 break;
             case 2:
-                _image.color = Color.yellow;
+                image.color = Color.yellow;
                 break;
             case 3:
-                _image.color = Color.green;
+                image.color = Color.green;
                 break;
         }
     }
+    
+    private void PauseMenu(){
+        //TODO 2 Criar menu principal e de pausa
+        //TODO 4 habilitar reload automatico(arcademode) ou munição para coletar(tankmode) no menu
+    }
+        
+    //TODO 1 Adicionar animação de explosão do kit do Kenney. Removendo as particulas que inseri.
+    //TODO 5 Alterar tipo de reload baseado na escolha do jogador
 }
