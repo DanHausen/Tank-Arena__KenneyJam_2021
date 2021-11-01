b8bbdc75834e2b8b6172ae67dd08cc2efe0c9848
Danhausen <danihausen@live.com>
2021-11-01 18:58:06 -0300

    WaveSpawner_woi

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -3,7 +3,36 @@ using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour
 {
+    public enum SpawnState{SPAWNING, WAITING, COUNTING};
+    
+    [System.Serializable]
     public class Wave{
-        
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+    }
+    
+    public Wave[] waves;
+    public int nextWave = 0;
+    
+    public float timeBetweenWaves = 5f;
+    public float waveCountdown;
+    
+    private SpawnState state = SpawnState.COUNTING;
+    
+    void Start(){
+        waveCountdown = timeBetweenWaves;
+    }
+    
+    void Update(){
+        if(waveCountdown <= 0){
+            if(state != SpawnState.SPAWNING){
+                // Spawn wave
+            }
+        }
+        else{
+            waveCountdown -= Time.deltaTime;
+        }
     }
 }
